a6efd301042fe93e045798abc8ff790ad2cf306e
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-05-19 23:38:05 +0600

    myToys get and delete done

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
-
-
-app.get('/', (req, res) => {
-    res.send('toy market running')
-
-})
-
 
 
 
@@ -35,6 +28,28 @@ async function run() {
         await client.connect();
         const usersCollection = client.db('shopCategory').collection('category')
         const toyCollection = client.db('toyCategory').collection('categories')
+        const myToyCollection = client.db('myToyCategory').collection('categories')
+
+        const indexKeys = { seller: 1, toyName: 1 };
+        const indexOptions = { name: "toyCategory" };
+        const result = await toyCollection.createIndex(indexKeys, indexOptions)
+
+        app.get('/toySearchBySeller/:text', async (req, res) => {
+            const searchText = req.params.text
+            console.log(searchText);
+
+            const result = await toyCollection.find({
+                $or: [
+                    { seller: { $regex: searchText, $options: "i" } },
+                    { toyName: { $regex: searchText, $options: "i" } },
+                ],
+
+            }).toArray()
+            res.send(result)
+        })
+
+
+
 
         app.get('/category', async (req, res) => {
             const cursor = usersCollection.find()
@@ -43,19 +58,48 @@ async function run() {
         })
 
         app.get('/users', async (req, res) => {
-            const cursor = toyCollection.find()
+            console.log(req.query);
+            const cursor = toyCollection.find(req.query).limit(20)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/myToys', async (req, res) => {
+            console.log(req.query.name);
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const cursor = myToyCollection.find(query)
             const result = await cursor.toArray();
             res.send(result)
         })
 
-
-
-        app.post('/users', async (req, res) => {
+        app.post('/myToys', async (req, res) => {
             const user = req.body
             console.log('create new', user);
-            const result = await toyCollection.insertOne(user)
+            const result = await myToyCollection.insertOne(user)
             res.send(result)
         })
+
+        app.delete('/myToys/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await myToyCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
 
 
 
@@ -70,6 +114,12 @@ async function run() {
 }
 run().catch(console.dir);
 
+
+
+app.get('/', (req, res) => {
+    res.send('toy market running')
+
+})
 
 app.listen(port, () => {
     console.log(`toy server running on ${port}`);
